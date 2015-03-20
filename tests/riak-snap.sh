6ecf2db76cd0b14962492059ff51f9586e125d2a
cornellio <peter.cornell@me.com>
2015-03-20 11:33:25 -0700

    update syntax

--- tests/riak-snap.sh
+++ tests/riak-snap.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
+#
+# A bare bones shell script to aide in testing a Puppet manifest for Riak
+# Can revert or validate a Riak install on RHEL ditros
+# Assumes sudo requiretty disabled on remote hosts and sudo as root enabled
+#
+# https://github.com/Cornellio/puppet-riak
 
-HOSTLIST="ppriakops01-sc9 \
-  ppriakops02-sc9 ppriakops03-sc9"
+HOSTLIST="ppriakops01-sc9 ppriakops02-sc9 ppriakops03-sc9"
 
 case $1 in
 
   remove)
-    for host in $HOSTLIST ; do
+    for host in $HOSTLIST; do
       printf "\nREMOVING\nHOST: $host\n"
       ssh -q -t $host "sudo yum -y remove riak erlang-rebar ; sudo rm -fr /etc/riak /var/lib/riak"
     done
     ;;
 
   check)
-    for host in $HOSTLIST ; do
+    for host in $HOSTLIST; do
       printf "\nCHECKING\nHOST: $host\n"
       ssh -q -t $host "/sbin/service riak status; ls /var/lib/riak"
       ssh -q -t $host "sudo /usr/sbin/riak-admin test"
@@ -23,7 +28,7 @@ case $1 in
     ;;
 
   *)
-    echo "give me something. 1 arg needed."
+    echo "Give something I can use. Try again"
     ;;
 
 esac
